a6f22eb486c1bced33af1af0eda6f355261ff55b
codinngar <codinngar@gmail.com>
2025-06-10 03:49:20 +0300

    Fix images

--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -1,5 +1,5 @@
-import  { Badge } from "./ui/badge";
-import  {
+import { Badge } from "./ui/badge";
+import {
   Card,
   CardHeader,
   CardDescription,
@@ -10,18 +10,24 @@ import  {
 const Post = () => {
   return (
     <Card>
-      <img src="https://placehold.co/300x200" className="rounded-xl" />
+      <div className="h-[200px]">
+        <img
+          src="https://cdn.pixabay.com/photo/2025/04/30/20/26/armillary-9570057_960_720.jpg"
+          className="w-full h-full object-cover rounded-xl"
+          alt="Post image"
+        />
+      </div>
       <div className="flex flex-col gap-4">
-        <CardHeader className="flex flex-col gap-4">
+        <CardHeader className="p-0">
           <CardDescription>
             <Badge variant="secondary">Technology</Badge>
           </CardDescription>
-          <CardTitle className="text-2xl font-bold cursor-pointer">
+          <CardTitle className="text-2xl font-bold cursor-pointer mt-2">
             Exploring the Future of AI in Modern Technology Trends
           </CardTitle>
         </CardHeader>
-        <CardContent>
-          <p>
+        <CardContent className="p-0">
+          <p className="text-muted-foreground">
             Discover how AI is transforming industries and learn about the
             latest advancements in artificial intelligence. Discover how AI is
             transforming industries and learn about the latest advancements in
