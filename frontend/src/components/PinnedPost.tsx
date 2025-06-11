5d2111757eabe61c824025353e8572771f1bc489
codinngar <codinngar@gmail.com>
2025-06-11 17:00:44 +0300

    Revert "Fix images"
    This reverts commit a6f22eb486c1bced33af1af0eda6f355261ff55b.

--- frontend/src/components/PinnedPost.tsx
+++ frontend/src/components/PinnedPost.tsx
@@ -12,10 +12,10 @@ const PinnedPost = () => {
     <div className="flex flex-col gap-8">
       <h2 className="text-3xl font-bold">Pinned Posts</h2>
       <Card className="flex flex-col md:flex-row">
-        <div className="h-[200px] md:h-[350px] md:w-1/2 lg:w-3/5">
+        <div className="md:w-1/2 lg:w-2/5">
           <img
-            src="https://cdn.pixabay.com/photo/2025/04/30/20/26/armillary-9570057_960_720.jpg"
-            className="border w-full h-full object-cover rounded-xl"
+            src="https://placehold.co/300x200"
+            className="w-full h-full object-cover rounded-t-xl md:rounded-l-xl md:rounded-tr-none"
           />
         </div>
         <div className="flex flex-col justify-center gap-4 p-6 md:w-1/2 lg:w-3/5">
--- frontend/src/components/Post.tsx
+++ frontend/src/components/Post.tsx
@@ -1,5 +1,5 @@
-import { Badge } from "./ui/badge";
-import {
+import  { Badge } from "./ui/badge";
+import  {
   Card,
   CardHeader,
   CardDescription,
@@ -10,24 +10,18 @@ import {
 const Post = () => {
   return (
     <Card>
-      <div className="h-[200px]">
-        <img
-          src="https://cdn.pixabay.com/photo/2025/04/30/20/26/armillary-9570057_960_720.jpg"
-          className="w-full h-full object-cover rounded-xl"
-          alt="Post image"
-        />
-      </div>
+      <img src="https://placehold.co/300x200" className="rounded-xl" />
       <div className="flex flex-col gap-4">
-        <CardHeader className="p-0">
+        <CardHeader className="flex flex-col gap-4">
           <CardDescription>
             <Badge variant="secondary">Technology</Badge>
           </CardDescription>
-          <CardTitle className="text-2xl font-bold cursor-pointer mt-2">
+          <CardTitle className="text-2xl font-bold cursor-pointer">
             Exploring the Future of AI in Modern Technology Trends
           </CardTitle>
         </CardHeader>
-        <CardContent className="p-0">
-          <p className="text-muted-foreground">
+        <CardContent>
+          <p>
             Discover how AI is transforming industries and learn about the
             latest advancements in artificial intelligence. Discover how AI is
             transforming industries and learn about the latest advancements in
